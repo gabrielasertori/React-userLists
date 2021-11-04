6e90a274fe1618df5a4552e426ae9430a9930085
gabriela <gabriela@sertori.com.br>
2021-11-04 14:59:17 -0300

    setting up environment and creating directories

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	mode: 'development',
+	devtool: 'eval-source-map',
+	entry: path.resolve(__dirname, 'src', 'index.jsx'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public', 'index.html')
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				use: ['style-loader', 'css-loader'],
+			}
+		]
+	},
+};
