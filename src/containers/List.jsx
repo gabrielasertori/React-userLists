e221c7f5bc6bc911f35057beaa6ed8c71aefe51e
gabriela <gabriela@sertori.com.br>
2021-11-06 14:19:05 -0300

    refactoring list

--- src/containers/List.jsx
+++ src/containers/List.jsx
@@ -1,56 +1,35 @@
-import { useState } from "react";
+/* V. Fetch API
+** V. Set 2 lists (Original and Sliced)
+** 3. Set slice function
+** 4. Map sliced list
+** 5. Set pagination button
+** 6. Implement functionality to button
+** 7. Set searchbar
+** 8. Implement functionality to searchbar
+** 9. Set sort by column
+** 10. Implement functionality to sort
+** 11. Style componentes
+** 12. Style list
+** 13. Style page
+*/
 
-export function List({ list }) {
-	const [search, setSearch] = useState("");
-	const [order, setOrder] = useState(1);
-	const [columnOrder, setColumnOrder] = useState('name');
+import { useEffect, useState } from "react"
 
-	const itensPerPage = (item, index) => index < 50;
 
-	const searchItem = (item, index) => {
-		if (search === "") {
-			return item
-		} else if (item.name.toLowerCase().startsWith(search.toLowerCase())) {
-			return item
-		} else if (item.age.toString() === search) {
-			return item
-		}
+const List = () => {
+	const [originalList, setOriginalList] = useState([]);
+	const [slicedList, setSlicedList] = useState([]);
+
+	const fetchApi = () => {
+		fetch("https://random-persons.herokuapp.com/users")
+			.then(res => res.json())
+			.then(data => {
+				setOriginalList(data.data)
+				setSlicedList(sliceList(data.data))
+			})
 	}
 
-	const ordenate = (columnName) => {
-		setOrder(-order);
-		setColumnOrder(columnName)
-	}
-
-	list = list.sort((a, b) => {
-		return a[columnOrder] < b[columnOrder] ? -order : order;
-	})
-
-	return (
-		<div>
-			<input
-				type="text"
-				placeholder="pesquisar"
-				onChange={e => setSearch(e.target.value)}
-			/>
-			<table>
-				<thead>
-					<tr>
-						<th onClick={e => ordenate('name')}>Nome</th>
-						<th onClick={e => ordenate('age')}>Idade</th>
-					</tr>
-				</thead>
-				<tbody>
-				{list.filter(searchItem).map( (item, index) => {
-						return (
-							<tr key={index}>
-								<td>{item.name}</td>
-								<td>{item.age}</td>
-							</tr>
-						)
-				})}
-				</tbody>
-			</table>
-		</div>
-	)
+	useEffect(() => {
+		fetchApi();
+	}, [])
 }
