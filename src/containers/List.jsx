cf8d27a4a384d9ce58ff7aed063667cfab00ce65
gabriela <gabriela@sertori.com.br>
2021-11-08 15:00:11 -0300

    implemented searchbar

--- src/containers/List.jsx
+++ src/containers/List.jsx
@@ -2,10 +2,10 @@
 ** V. Set 2 lists (Original and Sliced)
 ** V. Set slice function
 ** V. Map sliced list
-** 5. Set pagination button
-** 6. Implement functionality to button
-** 7. Set searchbar
-** 8. Implement functionality to searchbar
+** V. Set pagination button
+** V. Implement functionality to button
+** V. Set searchbar
+** V. Implement functionality to searchbar
 ** 9. Set sort by column
 ** 10. Implement functionality to sort
 ** 11. Style componentes
@@ -15,12 +15,15 @@
 
 import { useEffect, useState } from "react"
 import Button from "../components/Button";
+import TableCells from "../components/TableCells";
+import Searchbar from "./Searchbar";
 import '../styles/global.css'
 
 
 const List = () => {
 	const [originalList, setOriginalList] = useState([]);
 	const [slicedList, setSlicedList] = useState([]);
+	const [searchList, setSearchList] = useState([]);
 	const [currentPage, setCurrentPage] = useState(1);
 	const [itensPerPage, setItensPerPage] = useState(50);
 
@@ -44,7 +47,7 @@ const List = () => {
 		return currentItems;
 	}
 
-	// ===> Paginate the page <===
+	// ===> Paginate <===
 	const previousPage = () => {
 		setCurrentPage(currentPage - 1);
 	}
@@ -61,35 +64,75 @@ const List = () => {
 		} else {
 			previousButton.classList.remove("hidden");
 		}
-		setSlicedList(sliceList(originalList));
+
+		if (searchList.length === 0) {
+			setSlicedList(sliceList(originalList));
+		} else {
+			setSlicedList(sliceList(searchList));
+		}
 	}, [currentPage])
+	// <===>
+
+	// ===> Searchbar <===
+	const searchBar = (search) => {
+		const searchSwap = [];
+
+		if (search === "") {
+			setSearchList(originalList);
+		} else {
+			originalList.filter(item => {
+				if (item.name.toLowerCase().startsWith(search.toLowerCase())
+					|| item.age.toString() === search) {
+					searchSwap.push(item);
+					setSearchList(searchSwap);
+				}
+			})
+		}
+	}
+
+	useEffect(() => {
+		setSlicedList(sliceList(searchList));
+	}, [searchList])
 	// <===>
 
 	return (
 		<div>
 			<h2>Lista de Usuários</h2>
+			<Searchbar
+				placeholder="Pesquisar..."
+				onChange={(e) => searchBar(e.target.value)}
+			/>
 			<table>
 				<thead>
 					<tr>
+						<th>Id</th>
 						<th>Nome</th>
 						<th>Idade</th>
 					</tr>
 				</thead>
 				<tbody>
-					{
-						slicedList.map((item, index) =>
-							(
-								<tr key={index}>
-								<td>{item.name}</td>
-								<td>{item.age}</td>
-								</tr>
-							)
+					{slicedList.map((item, index) => {
+						return (
+							<tr key={index}>
+								<TableCells
+									index={index}
+									name={item.name}
+									age={item.age}/>
+							</tr>
 						)
+					})
 					}
 				</tbody>
 			</table>
-			<Button className="previous hidden" paginate={previousPage}>Anterior</Button>
-			<Button paginate={nextPage}>Próximo</Button>
+			<Button
+				className="previous hidden"
+				paginate={previousPage}>
+					Anterior
+			</Button>
+			<Button
+				paginate={nextPage}>
+					Próximo
+			</Button>
 		</div>
 	)
 }
